98402723f3132c30f23926c9502260062cc069a8
Kaktus1549 <hummelotup@seznam.cz>
2024-11-11 07:57:36 +0100

    Update daily_clear_attempts event start date to November 12, 2024

--- SQL/6_Scheduler.sql
+++ SQL/6_Scheduler.sql
@@ -2,7 +2,7 @@ USE Wordle;
 SET GLOBAL event_scheduler = ON;
 CREATE EVENT IF NOT EXISTS daily_clear_attempts
 ON SCHEDULE EVERY 1 DAY
-STARTS '2024-11-04 00:00:00'
+STARTS '2024-11-12 00:00:00'
 DO
     DELETE FROM `Wordle`.`AttemptsHistory`;
     DELETE FROM `Wordle`.`Attempts`;
