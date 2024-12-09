2724978b580285b96dcdc8516d83a899facf062d
Kaktus1549 <hummelotup@seznam.cz>
2024-12-09 13:18:15 +0100

    Rename daily_clear_attempts event to daily_clear_attemptsHistory and update start date to December 10, 2024

--- SQL/6_Scheduler.sql
+++ SQL/6_Scheduler.sql
@@ -1,8 +1,12 @@
 USE Wordle;
 SET GLOBAL event_scheduler = ON;
-CREATE EVENT IF NOT EXISTS daily_clear_attempts
+CREATE EVENT IF NOT EXISTS daily_clear_attemptsHistory
 ON SCHEDULE EVERY 1 DAY
-STARTS '2024-11-12 00:00:00'
+STARTS '2024-12-10 00:00:00'
 DO
     DELETE FROM `Wordle`.`AttemptsHistory`;
+CREATE EVENT IF NOT EXISTS daily_clear_attempts
+ON SCHEDULE EVERY 1 DAY
+STARTS '2024-12-10 00:00:00'
+DO  
     DELETE FROM `Wordle`.`Attempts`;
